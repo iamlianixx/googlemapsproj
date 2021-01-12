783929a9381e7386bfe744007900386114806be7
Jullian Robin Sibi <jullian.sibi@gmail.com>
2021-01-12 11:45:12 +0000

    Added static files and initial app files

--- googlemapsproj/googlemapsproj/settings.py
+++ googlemapsproj/googlemapsproj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gmapsmain'
 ]
 
 MIDDLEWARE = [
@@ -76,9 +78,10 @@ WSGI_APPLICATION = 'googlemapsproj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': '/home/julliansibi/gmapsproj/googlemapsproj/googlemapsproj/gmapssql.cnf',
-        },
+        'HOST': 'julliansibi.mysql.pythonanywhere-services.com',
+        'NAME': 'julliansibi$googlemapsapp',
+        'USER': 'julliansibi',
+        'PASSWORD': 'pyany123'
     }
 }
 
@@ -120,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
