b04ee67c4a0ac8a02e575bd4198d8cccf57bcc2f
Jullian Robin Sibi <jullian.sibi@gmail.com>
2021-01-13 08:07:52 +0000

    Added some functionality to highlight

--- googlemapsproj/gmapsmain/static/js/index.js
+++ googlemapsproj/gmapsmain/static/js/index.js
@@ -1,8 +1,38 @@
 let map;
+var renderedCircle;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    zoom: 12,
   });
+
+  if (navigator.geolocation) {
+     navigator.geolocation.getCurrentPosition(function (position) {
+         var initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+         map.setCenter(initialLocation);
+     });
+ }
 }
+
+function renderCircle() {
+    
+    if(document.getElementById("areaCheck").checked) {
+        renderedCircle = new google.maps.Circle({
+            strokeColor: "#FF0000",
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: "#FF0000",
+            fillOpacity: 0.35,
+            map,
+            center: map.getCenter(),
+            radius: 5000,
+        });
+    
+    } else {
+        if(renderedCircle != null) {
+            renderedCircle.setMap(null);
+            renderedCircle = null;
+        }
+    }
+}
+
